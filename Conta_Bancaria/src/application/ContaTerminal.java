427cca523c3545a2ceb6bd98e73809d21f35610a
Alberto <alberto.cardoso@globalhitss.com.br>
2023-09-19 09:39:49 -0300

    Primeiro commit

---
+++ Conta_Bancaria/src/application/ContaTerminal.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int numero;
+		String agencia;
+		String nomeCliente;
+		Double saldo;
+	
+	
+		System.out.println("Por favor, digite o número da Agência !");
+		 agencia = sc.nextLine();
+		 
+		 System.out.println(agencia);
+
+	}
+
+}
