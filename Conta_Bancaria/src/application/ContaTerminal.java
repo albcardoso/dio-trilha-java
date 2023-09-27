5f2032f8b0c8d9612cd049156e11c852bd70c151
Alberto <alberto.cardoso@globalhitss.com.br>
2023-09-27 14:59:01 -0300

    Versão final

--- Conta_Bancaria/src/application/ContaTerminal.java
+++ Conta_Bancaria/src/application/ContaTerminal.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class ContaTerminal {
 
 	public static void main(String[] args) {
+		//TODO Conhecer e importar a classe Scanner
+		//TODO Exibir as mensagens para o nosso usuario
+		//TODO Obter pela scanner os valores digitados no terminal
+		//TODO Exibir a mensagem da conta criada
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int numero;
@@ -13,10 +18,22 @@ public class ContaTerminal {
 		Double saldo;
 	
 	
-		System.out.println("Por favor, digite o número da Agência !");
-		 agencia = sc.nextLine();
+		 System.out.println("Por favor, digite o número da Conta!");
+		 numero = sc.nextInt();	 	 
 		 
-		 System.out.println(agencia);
+		 System.out.println("Por favor, digite o número da Agência !");
+		 agencia = sc.next();
+		 
+		 System.out.println("Por favor, digite o seu nome !");
+		 nomeCliente = sc.next();
+		 
+		 System.out.println("Por favor, digite o saldo desejado !");
+		 saldo = sc.nextDouble();
+		 
+		 System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é = " + agencia + ", conta de numero " + numero + " e o seu saldo de "+ "R$" + saldo + 
+				 " já esta disponível para saque");
+		 
+		 sc.close();
 
 	}
 
--- Conta_Bancaria/src/module-info.java
+++ Conta_Bancaria/src/module-info.java
@@ -1,8 +1,3 @@
-/**
- * 
- */
-/**
- * 
- */
+
 module Conta_Bancaria {
 }
